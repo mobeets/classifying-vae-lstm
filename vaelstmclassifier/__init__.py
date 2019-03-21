83738cade96dec20f1094c72b0008facba2a3a83
Jonathan <jdfraine@gmail.com>
2019-03-21 12:02:03 +0200

    updated subpackage __init__ setup

--- vaelstmclassifier/__init__.py
+++ vaelstmclassifier/__init__.py
@@ -1,3 +1,3 @@
 from . import vae_classifier
 from . import vaelstm_classifier
-from . import utils
+from .utils import midi_utils, model_utils, pianoroll, weightnorm
---
+++ vaelstmclassifier/utils/__init__.py
@@ -0,0 +1,1 @@
+from . import midi_utils, model_utils, pianoroll, weightnorm
